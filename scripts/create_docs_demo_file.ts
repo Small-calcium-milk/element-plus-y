1ab2cf9c9347784877ec32e8a751c217e016d21f
ybl <592882650@qq.com>
2024-01-13 19:22:32 +0800

    修改自动创建docs_demo文件

--- scripts/create_docs_demo_file.ts
+++ scripts/create_docs_demo_file.ts
@@ -14,6 +14,16 @@ async function main() {
   const content = await readFile(files[0], 'utf-8')
   // consola.log(chalk.cyan(content))
 
+  const demoFile = await glob("demo.vue", {
+    cwd: path.resolve(docRoot, 'examples/test/components'),
+    absolute: true,
+    onlyFiles: true,
+  })
+  if(demoFile.length) {
+    consola.success(chalk.green(`demo.vue 已存在,不再覆盖,结束`))
+    return
+  }
+
   await mkdir(path.dirname(path.resolve(docRoot, 'examples/test/components/demo.vue')), {
     recursive: true,
   })
