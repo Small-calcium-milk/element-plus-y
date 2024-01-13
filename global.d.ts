e5a51848684051ac7e6ac6ccbf9118ba9fe8b32e
ybl <592882650@qq.com>
2024-01-13 19:55:23 +0800

    fix

--- global.d.ts
+++ global.d.ts
@@ -4,7 +4,7 @@
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     YCascader: typeof import('@small_calcium_milk/element-plus-y')['YCascader']
-
+    YPasswordStrength: typeof import('@small_calcium_milk/element-plus-y')['YPasswordStrength']
   }
 
   interface ComponentCustomProperties {
--- packages/element-plus/component.ts
+++ packages/element-plus/component.ts
@@ -1,8 +1,6 @@
 import { YCascader } from '@element-plus/components/cascader'
-
+import { YPasswordStrength } from '@element-plus/components/passwordStrength'
 
 import type { Plugin } from 'vue'
 
-export default [
-  YCascader,
-] as Plugin[]
+export default [YCascader, YPasswordStrength] as Plugin[]
